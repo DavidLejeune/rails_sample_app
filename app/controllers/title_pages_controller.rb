81225fff83bee48c9726b4b44e73d535c955f5e1
DavidLejeune <Lejeune.David@outlook.com>
2017-10-05 19:33:54 +0000

    TitlePages'

---
+++ app/controllers/title_pages_controller.rb
@@ -0,0 +1,19 @@
+class TitlePagesController < ApplicationController
+  def home
+  end
+
+  def personal
+  end
+
+  def professional
+  end
+
+  def code
+  end
+
+  def school
+  end
+
+  def contact
+  end
+end
