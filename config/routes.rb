4383ffb72b2aeccf9c59d5a5027832df7cb130c5
DavidLejeune <Lejeune.David@outlook.com>
2017-10-06 07:53:30 +0000

    reset 2 of the day

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  #get 'welcome/index'
-
-  #get 'welcome/sample'
-	  match ':controller(/:action(/:id))', :via => :get
-
-
-  root 'welcome#index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
