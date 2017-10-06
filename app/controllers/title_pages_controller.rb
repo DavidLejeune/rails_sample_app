fe791b891c070c927b9fe13a6337dc5ce4345e13
DavidLejeune <Lejeune.David@outlook.com>
2017-10-06 08:01:47 +0000

    TitlePages

---
+++ app/controllers/title_pages_controller.rb
@@ -0,0 +1,22 @@
+class TitlePagesController < ApplicationController
+  def home
+  end
+
+  def personal
+  end
+
+  def professional
+  end
+
+  def code
+  end
+
+  def school
+  end
+
+  def misc
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'title_pages/home'
+
+  get 'title_pages/personal'
+
+  get 'title_pages/professional'
+
+  get 'title_pages/code'
+
+  get 'title_pages/school'
+
+  get 'title_pages/misc'
+
+  get 'title_pages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
