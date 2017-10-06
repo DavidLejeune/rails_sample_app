601cdbb3235a5665ef1bf8f4224dfe21b03229c5
DavidLejeune <Lejeune.David@outlook.com>
2017-10-06 07:00:31 +0000

    sub pages can be reached from port 80 now

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  #get 'welcome/index'
 
-  get 'welcome/sample'
+  #get 'welcome/sample'
+	  match ':controller(/:action(/:id))', :via => :get
+
+
+  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
